d92f23936c504b9a92fef06654e536db72831653
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-05-13 22:09:31 +0200

    Add chevron icon svg

---
+++ lib/components/Icons/ChevronDown.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- All credit to Dazzle UI  https://www.svgrepo.com/svg/533653/chevron-down -->
+  <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+  <svg
+    class="bk-icon"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24px"
+    height="24px"
+    alt="chevron down icon"
+  >
+    <path
+      d="M6 9L12 15L18 9"
+      stroke="#000000"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+<style>
+.bk-icon {
+  width: 24px;
+  height: 24px;
+}
+</style>
