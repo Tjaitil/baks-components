44be956f5c4721cdb66bf9b67769f9a28dc37c08
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-06-03 22:59:06 +0200

    Add package with shared functionality

---
+++ packages/shared/index.ts
@@ -0,0 +1,5 @@
+import type { ThemeVariant, ThemeVariants } from './src/types/ThemeVariants';
+import { resolveVariant, variantsOptions } from './src/utilities/variants';
+
+export type { ThemeVariant, ThemeVariants };
+export { resolveVariant, variantsOptions };
