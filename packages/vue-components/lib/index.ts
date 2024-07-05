dbcd4e3323820d24f8a0f22f19c2032d0e84f19f
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-07-05 21:06:46 +0200

    fix(baks-components-vue): fix missing component exports

--- packages/vue-components/lib/index.ts
+++ packages/vue-components/lib/index.ts
@@ -1,6 +1,9 @@
 import BaksButton from './components/baks-button/BaksButton.vue';
 import BaksAccordion from './components/baks-accordion/BaksAccordion.vue';
+import BaksTab from './components/baks-tabs/BaksTab.vue';
+import BaksTabPanel from './components/baks-tabs/BaksTabPanel.vue';
+import BaksCard from './components/baks-card/BaksCard.vue';
 import './app.css';
 export type { ThemeVariant as ThemeVariants } from 'baks-components-styles';
 
-export { BaksButton, BaksAccordion };
+export { BaksButton, BaksAccordion, BaksTab, BaksTabPanel, BaksCard };
