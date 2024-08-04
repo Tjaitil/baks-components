f4da4055fe1710372edfbe844151b09261096fc6
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-08-04 20:27:52 +0200

    chore: fix issue causing build to fail

--- packages/web-components/src/main.ts
+++ packages/web-components/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import { register } from '../lib';
 import App from './App.vue';
-import { resolveVariant } from '@baks-components/shared';
 
 register();
 const app = createApp(App);
