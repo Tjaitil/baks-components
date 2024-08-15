c7132609f7d2bf26ae96548bb6f65d9ecd11f265
Kjetil Baksaas <52608380+Tjaitil@users.noreply.github.com>
2024-08-15 18:54:14 +0200

    fix(BaksCard): missing bk-card classname

--- packages/vue-components/lib/components/baks-card/BaksCard.vue
+++ packages/vue-components/lib/components/baks-card/BaksCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     part="baks-card"
-    class="relative p-4 rounded shadow-sm shadow-black"
+    class="bk-card relative p-4 rounded shadow-sm shadow-black"
     :class="resolveVariant(variant)"
   >
     <slot></slot>
