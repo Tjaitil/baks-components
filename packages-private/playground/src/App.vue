9f59270ed56ab5ca2f2d0735069cfe0515b9b952
Kjetil Vea Baksaas <52608380+Tjaitil@users.noreply.github.com>
2025-07-20 11:13:04 +0200

    chore: add playground as private package (#37)

---
+++ packages-private/playground/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { BaksButton } from 'baks-components-vue';
+</script>
+
+<template>
+  <h1
+    class="text-3xl font-bold tracking-wide relative starting:right-8 right-0 transition-all my-6 px-6"
+  >
+    Component playground
+  </h1>
+  <BaksButton variant="primary">Click me!</BaksButton>
+</template>
+
+<style scoped></style>
