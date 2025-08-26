8dfb0f58d492487759be8456782e07576b9f7984
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-08-26 22:06:24 +0200

    chore: add codePanel and show sourceStat

--- packages/web-components/.storybook/preview.ts
+++ packages/web-components/.storybook/preview.ts
@@ -5,6 +5,12 @@ import './style.css';
 const preview: Preview = {
   tags: ['autodocs'],
   parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown'
+      },
+      codePanel: true
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
