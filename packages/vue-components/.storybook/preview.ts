f68c062c784a128eb110c4b7a41ca8398eae0051
Kjetil Vea Baksaas <kjetil@baksaas.no>
2025-08-26 22:10:33 +0200

    chore: storybook add autodocs, sourceState and codePanel (#48)

--- packages/vue-components/.storybook/preview.ts
+++ packages/vue-components/.storybook/preview.ts
@@ -3,7 +3,14 @@ import type { Preview } from '@storybook/vue3-vite';
 import '../app.css';
 
 const preview: Preview = {
+  tags: ['autodocs'],
   parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown'
+      },
+      codePanel: true
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- packages/web-components/.storybook/preview.ts
+++ packages/web-components/.storybook/preview.ts
@@ -3,7 +3,14 @@ import type { Preview } from '@storybook/web-components-vite';
 import './style.css';
 
 const preview: Preview = {
+  tags: ['autodocs'],
   parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown'
+      },
+      codePanel: true
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
